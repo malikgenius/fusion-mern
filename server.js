730476755adc9463f16f647820a8396287396a85
malikgenius <malikgen2010@gmail.com>
2018-10-30 14:10:40 +0400

    Testing public folder in node.js for images- second.

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ app.use('/api/upload', upload);
 app.use(express.static('public'));
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.use(express.static('public'));
+  app.use(express.static('client/build', 'public'));
+  // app.use(express.static('public'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
