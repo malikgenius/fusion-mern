f4d1a13c819646061c03aa6b55c24f606dc17d93
malikgenius <malikgen2010@gmail.com>
2018-10-30 22:00:33 +0400

    Server Public folder disabled and full search working

--- server.js
+++ server.js
@@ -53,13 +53,13 @@ app.use('/api/reset', reset_verify);
 app.use('/api/stock', stock);
 app.use('/api/upload', upload);
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // if we use multer to save images in public folder, thats how we will access them from the client.
   // multer should save the imageurl address similar to this.
   //https://sheltered-anchorage-84432.herokuapp.com/1.jpg     where 1.jpg is saved in public folder or any other we specify.
-  app.use(express.static('public'));
+  // app.use(express.static('public'));
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
